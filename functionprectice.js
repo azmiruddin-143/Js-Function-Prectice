3e0ed2a1864383ea074476fbb0d86c6b5dc94ff7
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-06 23:43:47 +0600

    first commit

---
+++ functionprectice.js
@@ -0,0 +1,130 @@
+
+//Function Prectice Start//
+
+// node functionprectice.js//
+
+
+// function number (num1,num2) {
+//     let mainas = num1-num2;
+//     console.log(mainas);
+
+// }
+// number(10,5);
+
+
+
+
+// node functionprectice.js//
+
+// function num (number1, number2, number3,number4){
+//      let plus = number1+number2;
+//      let mainas = number3-number4;
+//      console.log(plus);
+//      console.log(mainas);
+// }
+
+// num(50,60,200,180);
+
+
+// node functionprectice.js//
+
+
+// function num (namber1,number2) {
+//     let gun = namber1 * number2;
+//     console.log(gun);
+// }
+
+//   let first = 8;
+//   let secont = 9;
+
+//  let finalnumber = num(first,secont);
+
+
+// node functionprectice.js//
+
+
+// function test([]) {
+//    console.log(arr);
+// }
+
+// let arr = [265, 56, 56, 56, 56,]
+// test(arr);
+
+
+
+// node functionprectice.js//
+// function az([]) {
+
+//     let sum = 0;
+//     for (let i = 0; i < ar.length; i++){
+
+//         if(ar[i]){
+//             sum=sum+ar[i]
+//         };
+
+//     }   
+//     console.log(sum);  
+// }
+
+// let ar = [54, 54, 546, 2, 3, 895,]
+// az(ar)
+
+
+
+
+// node functionprectice.js//
+
+function plus([]) {
+    let total = 0;
+    for (let i = 0; i < arry.length; i++) {
+        if (arry[i] % 2 === 0) {
+            total = total + arry[i];
+        }
+    }
+    console.log("jogfol :",total,"taka");
+    return total;
+}
+
+let arry = [10, 54, 45, 8, 9, 54, 30, 77, 100, 90, 87, 99]
+plus(arry)
+
+
+// node functionprectice.js//
+
+// let bosto = {
+//     nam: "Azmir Uddin",
+//     country: "Bangladesh",
+//     city: "Dhaka",
+//     PhoneNuymber: 1933946077,
+//     profession: "Web Developer"
+// }
+
+// function ob(az) {
+
+//     for(let gi in az ){ 
+//        console.log(az[gi]);
+//     }
+   
+// }
+
+// ob(bosto)
+
+
+
+
+    //  sapport section help//
+
+// function sendEmail(name, body) {
+//     console.log(`${name} send you an email,
+//     Email Body: ${body}`)
+//   }
+  
+//   function sayHello(name) {
+//     return "Hi, " + name;
+//     console.log("Hi, " + name)
+//   }
+  
+//   sendEmail('nkecme', sayHello("nkecme"))
+//   sendEmail('ke', sayHello("ke"))
+
+ //  sapport section help//
